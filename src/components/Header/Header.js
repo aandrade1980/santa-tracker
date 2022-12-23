f9c7af1f1ad088e22849102a24cc0c2f357f8406
Alvaro <varito1@gmail.com>
2022-12-23 12:18:31 -0300

    :art: Updated github link.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -15,7 +15,8 @@ const Header = () => {
         <ul className={styles.headerLinks}>
           <li>
             <a
-              href="https://github.com/colbyfayock/next-leaflet-starter"
+              // href="https://github.com/colbyfayock/next-leaflet-starter"
+              href="https://github.com/aandrade1980/santa-tracker"
               rel="noreferrer"
             >
               <FaGithub />
